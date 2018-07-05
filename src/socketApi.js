693ed14a7d50562baf9819082324c154e9983f8d
Semih Duman <semihdumannn@gmail.com>
2018-07-05 16:45:00 +0300

    new User event

--- src/socketApi.js
+++ src/socketApi.js
@@ -1,10 +1,29 @@
 const  socketio = require('socket.io');
 const io = socketio();
-const socketApi = { };
 
+const socketApi = { };
 socketApi.io = io;
+
+const users = [ ];
+
 io.on('connection', (socket) => {
     console.log('a user connected');
+
+    socket.on('new-user', ( data ) => {
+        //console.log(data);
+        const defaultData = {
+          id : socket.id,
+          position : {
+              x : 0,
+              y : 0
+          }
+        };
+        const userData =  Object.assign(data,defaultData);
+        //console.log(userData);
+        users.push(userData);
+       // console.log(users);
+
+    });
 
 });
 
